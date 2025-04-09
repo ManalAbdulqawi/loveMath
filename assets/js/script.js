88b7347bb0fae22d99e96b96deefcc8e1f40edbb
ManalAbdulqawi <denever_2008@windowslive.com>
2025-04-09 11:28:34 +0100

    empty answer box and put focuse on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ for(let button of buttons)
 })
 
 }
+
+document.getElementById("answer-box").addEventListener("keydown",function(event){
+    if(event.key === "Enter"){
+        checkAnswer();
+    }
+})
 runGame("addition");
 
 })
@@ -23,7 +29,8 @@ runGame("addition");
  * and after the user's answer has been processed
 */
 function runGame(gameType)
-{
+{document.getElementById("answer-box").value = "";
+ document.getElementById("answer-box").focus();
     //create random numbers between 1 and 25
 let num1=Math.floor(Math.random()*25+1); // +1 to prevent division by 0
 let num2=Math.floor(Math.random()*25+1); 
@@ -56,7 +63,7 @@ function checkAnswer(){
         alert(`Awwww .... you answered ${userAnswer}. The correct answer was ${calulateAnswer[0]}!`);
         incrementWrongAnswer();
     }
-
+runGame(calulateAnswer[1])
 }
 /**
  * Gets the operands (the numbers) and the operator (plus, minus etc)
@@ -117,5 +124,5 @@ function displayMultiplyQuestion(operand1,operand2)
 {// we want the result of the subtact to be positive so the operand1 has to be bigger than operand2
     document.getElementById("operand1").textContent=operand1;
     document.getElementById("operand2").textContent=operand2;
-    document.getElementById("operator").textContent="-";
+    document.getElementById("operator").textContent="x";
 }
